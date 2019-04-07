967fa09e57634510b3f795bf2c8ef8467ff712fe
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-07 03:25:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ end
 
 def monopoly_with_third_tier
 	monopoly = {:railroads => {:pieces => 4,
-	                           :names => :reading_ralroad => {},
+	                           :names => {:reading_ralroad => {},
 	                                      :pennsylvania_railroad => {},
 	                                      :b_and_o_railroad => {},
 	                                      :shortline => {}
